d3d4da3498cb25407bbb9035ab2c52baabb86e19
Jon Bonazza <jonbonazza@gmail.com>
2018-08-01 21:11:33 -0700

    clean up database.gd

--- addons/godot-tools.dialog-system/database.gd
+++ addons/godot-tools.dialog-system/database.gd
@@ -3,11 +3,9 @@ tool
 extends Node
 
 const _VARS = "__vars__"
-const _CONDITIONS = "__cond__"
 
 var _mem = {
 	_VARS: {},
-	_CONDITIONS: {}
 }
 	
 func set_var(key, val):
@@ -15,13 +13,3 @@ func set_var(key, val):
 
 func get_var(key):
 	return _mem[_VARS][key]
-	
-func set_condition(id, cond):
-	_mem[_CONDITIONS][id] = cond
-
-func get_condition(id):
-	_mem[_CONDITIONS][id]
-
-
-
-
